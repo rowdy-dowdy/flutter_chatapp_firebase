4069ed47848d5f96cefa07d6d80fe2d04b134e6a
Your Name <you@example.com>
2023-03-10 10:42:23 +0700

    add

--- lib/pages/call/call_detail.dart
+++ lib/pages/call/call_detail.dart
@@ -112,7 +112,7 @@ class CallDetailNew extends ConsumerWidget {
               ),
             ),
           ),
-          !isMeCall ? Positioned(
+          isMeCall ? Positioned(
             bottom: 50,
             left: 50,
             right: 50,
